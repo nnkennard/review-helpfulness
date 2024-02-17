97b900f11c9c00bbb72b697002fd5e195d64aa37
Neha Kennard <nnayak@umass.edu>
2024-02-17 18:46:26 +0000

    still bad rseults

--- classification_trial.py
+++ classification_trial.py
@@ -9,17 +9,8 @@ import torch
 
 from peft import get_peft_model, LoraConfig, TaskType
 
-MyDataset = collections.namedtuple("MyDataset",
-                                   "name examples label2id".split())
-
-ACCURACY = evaluate.load('accuracy')
-
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return ACCURACY.compute(predictions=predictions, references=labels)
-
+#MyDataset = collections.namedtuple("MyDataset",
+#                                   "name examples label2id".split())
 
 def preprocess_function(examples, tokenizer):
     return tokenizer(examples["text"], truncation=True)
@@ -63,21 +54,31 @@ def train_and_eval_distillbert(dataset, tokenizer):
 
     trainer.train()
 
+
 def compute_metrics(eval_pred):
     # All metrics are already predefined in the HF `evaluate` package
     precision_metric = evaluate.load("precision")
     recall_metric = evaluate.load("recall")
-    f1_metric= evaluate.load("f1")
+    f1_metric = evaluate.load("f1")
     accuracy_metric = evaluate.load("accuracy")
 
-    logits, labels = eval_pred # eval_pred is the tuple of predictions and labels returned by the model
+    logits, labels = eval_pred  # eval_pred is the tuple of predictions and labels returned by the model
     predictions = np.argmax(logits, axis=-1)
-    precision = precision_metric.compute(predictions=predictions, references=labels)["precision"]
-    recall = recall_metric.compute(predictions=predictions, references=labels)["recall"]
-    f1 = f1_metric.compute(predictions=predictions, references=labels)["f1"]
-    accuracy = accuracy_metric.compute(predictions=predictions, references=labels)["accuracy"]
-    # The trainer is expecting a dictionary where the keys are the metrics names and the values are the scores. 
-    return {"precision": precision, "recall": recall, "f1-score": f1, 'accuracy': accuracy}
+    #precision = precision_metric.compute(predictions=predictions,
+    #                                     references=labels)["precision"]
+    #recall = recall_metric.compute(predictions=predictions,
+    #                               references=labels)["recall"]
+    #f1 = f1_metric.compute(predictions=predictions, references=labels)["f1"]
+    accuracy = accuracy_metric.compute(predictions=predictions,
+                                       references=labels)["accuracy"]
+    # The trainer is expecting a dictionary where the keys are the metrics names and the values are the scores.
+    return {
+     #   "precision": precision,
+      #  "recall": recall,
+       # "f1-score": f1,
+        'accuracy': accuracy
+    }
+
 
 def llama_preprocessing_function(examples, llama_tokenizer):
     MAX_LEN = 500
@@ -87,19 +88,40 @@ def llama_preprocessing_function(examples, llama_tokenizer):
 
 
 class WeightedCELossTrainer(Trainer):
-    def __init__(self):
-        # super()
-        # self.weights = weights # how??
-        raise NotImplementedError
-    def compute_loss(self, model, inputs, weights, return_outputs=False):
+
+    def __init__(self, model, args, train_dataset, eval_dataset, data_collator,
+    compute_metrics, weights):
+        super().__init__(
+            model=model,
+            args=args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics
+            )
+        self.weights = weights
+
+    def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.pop("labels")
         # Get model's predictions
         outputs = model(**inputs)
         logits = outputs.get("logits")
         # Compute custom loss
-        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor(weights, device=model.device, dtype=logits.dtype))
-        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor(
+            self.weights, device=model.device, dtype=logits.dtype))
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels),
+                        labels.view(-1))
         return (loss, outputs) if return_outputs else loss
+
+
+def calculate_weights(train_dataset):
+    weight_counts = collections.Counter(x['label'] for x in train_dataset)
+    total_samples = len(train_dataset)
+    num_classes = len(weight_counts)
+    print(weight_counts.keys())
+    assert set(weight_counts.keys()) == set(range(len(weight_counts)))
+    return [total_samples/(num_classes * weight_counts[i]) for i in
+    range(num_classes)]
 
 def llama(dataset, label2id):
     llama_checkpoint = "/datasets/ai/llama2/huggingface/llama-2-7b"
@@ -108,6 +130,7 @@ def llama(dataset, label2id):
     llama_tokenizer.pad_token_id = llama_tokenizer.eos_token_id
     llama_tokenizer.pad_token = llama_tokenizer.eos_token
 
+    weights = calculate_weights(dataset['train'])
     llama_tokenized_datasets = dataset.map(
         lambda x: llama_preprocessing_function(x, llama_tokenizer),
         batched=True)
@@ -116,7 +139,7 @@ def llama(dataset, label2id):
 
     llama_model = AutoModelForSequenceClassification.from_pretrained(
         pretrained_model_name_or_path=llama_checkpoint,
-        num_labels=2,
+        num_labels=len(weights),
         device_map="auto",
         offload_folder="offload",
         trust_remote_code=True)
@@ -124,12 +147,16 @@ def llama(dataset, label2id):
     llama_model.config.pad_token_id = llama_model.config.eos_token_id
 
     llama_peft_config = LoraConfig(
-    task_type=TaskType.SEQ_CLS, r=16, lora_alpha=16, lora_dropout=0.05, bias="none", 
-    target_modules=[
-        "q_proj",
-        "v_proj",  
-            ],
-        )
+        task_type=TaskType.SEQ_CLS,
+        r=16,
+        lora_alpha=16,
+        lora_dropout=0.05,
+        bias="none",
+        target_modules=[
+            "q_proj",
+            "v_proj",
+        ],
+    )
 
     llama_model = get_peft_model(llama_model, llama_peft_config)
     llama_model.print_trainable_parameters()
@@ -142,9 +169,9 @@ def llama(dataset, label2id):
     training_args = TrainingArguments(
         output_dir="llama-lora-token-classification",
         learning_rate=lr,
-        lr_scheduler_type= "constant",
-        warmup_ratio= 0.1,
-        max_grad_norm= 0.3,
+        lr_scheduler_type="constant",
+        warmup_ratio=0.1,
+        max_grad_norm=0.3,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         num_train_epochs=num_epochs,
@@ -157,47 +184,48 @@ def llama(dataset, label2id):
         gradient_checkpointing=True,
     )
 
-    #llama_trainer = WeightedCELossTrainer(
-    llama_trainer = Trainer(
+    llama_trainer = WeightedCELossTrainer(
+    #llama_trainer = Trainer(
         model=llama_model,
         args=training_args,
         train_dataset=llama_tokenized_datasets['train'],
         eval_dataset=llama_tokenized_datasets["dev"],
         data_collator=llama_data_collator,
         compute_metrics=compute_metrics,
+        weights=weights,
     )
 
     llama_trainer.train()
 
-
-    print("yay")
+def mixtral():
+    pass
 
 
 def main():
 
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-    with open('data/labeled/asp.pkl', 'rb') as f:
+    with open('data/labeled/pol.pkl', 'rb') as f:
         untokenized, label2id = pickle.load(f)
 
     llama(untokenized, label2id)
 
     exit()
 
-    max_char = untokenized['train'].to_pandas()['text'].str.len().max()
+    #max_char = untokenized['train'].to_pandas()['text'].str.len().max()
     # Number of Words
-    max_words = untokenized['train'].to_pandas()['text'].str.split().str.len(
-    ).max()
+    #max_words = untokenized['train'].to_pandas()['text'].str.split().str.len(
+    #).max()
 
-    print(max_char, max_words)
-    dsds
+    #print(max_char, max_words)
+    #dsds
 
     # Specific to imdb dataset
-    tokenized = untokenized.map(lambda x: preprocess_function(x, tokenizer),
+    #tokenized = untokenized.map(lambda x: preprocess_function(x, tokenizer),
                                 batched=True)
-    dataset = MyDataset("asp", tokenized, label2id)
+    #dataset = MyDataset("asp", tokenized, label2id)
 
-    train_and_eval_distillbert(dataset, tokenizer)
+    #train_and_eval_distillbert(dataset, tokenizer)
 
 
 if __name__ == "__main__":
